5550ed740de1502feff12889a89388dd44b15e0d
mcalamita <mcalamita@deloitte.it>
2025-06-27 01:19:59 +0200

    fix vercel json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
+import { ImportUtentiComponent } from './import-utenti/import-utenti.component';
+import { ListaUtentiComponent } from './lista-utenti/lista-utenti.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
+  imports:[ImportUtentiComponent, ListaUtentiComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatCardModule } from '@angular/material/card';
+import { ImportUtentiComponent } from './import-utenti/import-utenti.component';
+import { ListaUtentiComponent } from './lista-utenti/lista-utenti.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImportUtentiComponent,
+    ListaUtentiComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
