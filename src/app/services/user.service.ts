287d5cb5da8a7c4b3da6310be06f95f46fc66640
michele111calamita <michele.calamita3@gmail.com>
2025-07-16 01:20:25 +0200

    a

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 export interface User {
@@ -19,8 +20,18 @@ export class UserService {
   private apiUrl = `${environment.apiUrl}/api/users`;
 
   constructor(private http: HttpClient) {}
-
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.apiUrl);
+    return this.http.get<any[]>(this.apiUrl).pipe(
+      map(users => users.map(user => ({
+        id: user.id,
+        nome: user.nome || '',
+        cognome: user.cognome || '',
+        dataNascita: user.data_nascita || '',
+        luogoNascita: user.citta || '',
+        codiceFiscale: user.codice_fiscale || '',
+        numeroTessera: user.id?.toString() || '',
+        codiceSicurezza: ''
+      })))
+    );
   }
 }
